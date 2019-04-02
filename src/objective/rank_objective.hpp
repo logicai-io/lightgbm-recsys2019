3957a0e6e0403028c10f74af92930e319c8d5699
Paweł Jankiewicz <p.jankiewicz@gmail.com>
2019-04-02 12:02:48 +0200

    New objective - not working yet

--- src/objective/rank_objective.hpp
+++ src/objective/rank_objective.hpp
@@ -275,18 +275,6 @@ class LambdarankMRR: public ObjectiveFunction {
       Log::Fatal("Lambdarank tasks require query information");
     }
     num_queries_ = metadata.num_queries();
-    // cache inverse max DCG, avoid computation many times
-    inverse_max_dcgs_.resize(num_queries_);
-#pragma omp parallel for schedule(static)
-    for (data_size_t i = 0; i < num_queries_; ++i) {
-      inverse_max_dcgs_[i] = DCGCalculator::CalMaxDCGAtK(optimize_pos_at_,
-        label_ + query_boundaries_[i],
-        query_boundaries_[i + 1] - query_boundaries_[i]);
-
-      if (inverse_max_dcgs_[i] > 0.0) {
-        inverse_max_dcgs_[i] = 1.0f / inverse_max_dcgs_[i];
-      }
-    }
     // construct sigmoid table to speed up sigmoid transform
     ConstructSigmoidTable();
   }
@@ -299,14 +287,96 @@ class LambdarankMRR: public ObjectiveFunction {
     }
   }
 
-  inline void GetGradientsForOneQuery(const double* score,
+    inline void GetGradientsForOneQuery(const double* score,
+                                        score_t* lambdas, score_t* hessians, data_size_t query_id) const {
+        // get doc boundary for current query
+        const data_size_t start = query_boundaries_[query_id];
+        const data_size_t cnt =
+                query_boundaries_[query_id + 1] - query_boundaries_[query_id];
+        // get max DCG on current query
+        const double inverse_max_dcg = inverse_max_dcgs_[query_id];
+        // add pointers with offset
+        const label_t* label = label_ + start;
+        score += start;
+        lambdas += start;
+        hessians += start;
+        // initialize with zero
+        for (data_size_t i = 0; i < cnt; ++i) {
+            lambdas[i] = 0.0f;
+            hessians[i] = 0.0f;
+        }
+        // get sorted indices for scores
+        std::vector<data_size_t> sorted_idx;
+        for (data_size_t i = 0; i < cnt; ++i) {
+            sorted_idx.emplace_back(i);
+        }
+        std::stable_sort(sorted_idx.begin(), sorted_idx.end(),
+                         [score](data_size_t a, data_size_t b) { return score[a] > score[b]; });
+
+        // find the best index
+        int r = 0;
+        for (data_size_t i = 0; i < cnt; ++i) {
+            if (label[sorted_idx[i]] > 0.1f) {
+                r = i;
+                break;
+            }
+        }
+
+        // start accmulate lambdas by pairs
+        data_size_t i = r;
+        const data_size_t high = sorted_idx[i];
+        const double high_score = score[high];
+        double high_sum_lambda = 0.0;
+        double high_sum_hessian = 0.0;
+        for (data_size_t j = 0; j < i; ++j) {
+            // skip same data
+            if (i == j) { continue; }
+
+            const data_size_t low = sorted_idx[j];
+            const double low_score = score[low];
+
+            // only consider pair with different label
+//            if (high_label <= low_label || low_score == kMinScore) { continue; }
+
+            const double delta_score = high_score - low_score;
+
+            double delta_pair_MRR = (((1.0f/(static_cast<double>(j + 1.0f))) - (1.0f / (static_cast<double>(i + 1.0f)))));
+
+            // calculate lambda for this pair
+            double p_lambda = GetSigmoid(delta_score);
+            double p_hessian = p_lambda * (2.0f - p_lambda);
+            // update
+            p_lambda *= -delta_pair_MRR;
+            p_hessian *= 2 * delta_pair_MRR;
+            high_sum_lambda += p_lambda;
+            high_sum_hessian += p_hessian;
+            lambdas[low] -= static_cast<score_t>(p_lambda);
+            hessians[low] += static_cast<score_t>(p_hessian);
+        }
+        // update
+        lambdas[high] += static_cast<score_t>(high_sum_lambda);
+        hessians[high] += static_cast<score_t>(high_sum_hessian);
+
+        // if need weights
+        if (weights_ != nullptr) {
+            for (data_size_t i = 0; i < cnt; ++i) {
+                lambdas[i] = static_cast<score_t>(lambdas[i] * weights_[start + i]);
+                hessians[i] = static_cast<score_t>(hessians[i] * weights_[start + i]);
+            }
+        }
+
+//        printf("------------\n");
+//        for (data_size_t i = 0; i < cnt; ++i) {
+//            printf("%d %.4f %.4f %.4f %.4f\n", i, label[sorted_idx[i]], score[sorted_idx[i]], lambdas[sorted_idx[i]], hessians[sorted_idx[i]]);
+//        }
+    }
+
+    inline void GetGradientsForOneQueryBackup(const double* score,
               score_t* lambdas, score_t* hessians, data_size_t query_id) const {
     // get doc boundary for current query
     const data_size_t start = query_boundaries_[query_id];
     const data_size_t cnt =
       query_boundaries_[query_id + 1] - query_boundaries_[query_id];
-    // get max DCG on current query
-    const double inverse_max_dcg = inverse_max_dcgs_[query_id];
     // add pointers with offset
     const label_t* label = label_ + start;
     score += start;
@@ -317,75 +387,55 @@ class LambdarankMRR: public ObjectiveFunction {
       lambdas[i] = 0.0f;
       hessians[i] = 0.0f;
     }
-    // get sorted indices for scores
+//    // get sorted indices for scores
     std::vector<data_size_t> sorted_idx;
     for (data_size_t i = 0; i < cnt; ++i) {
       sorted_idx.emplace_back(i);
     }
     std::stable_sort(sorted_idx.begin(), sorted_idx.end(),
                      [score](data_size_t a, data_size_t b) { return score[a] > score[b]; });
-    // get best and worst score
-    const double best_score = score[sorted_idx[0]];
-    data_size_t worst_idx = cnt - 1;
-    if (worst_idx > 0 && score[sorted_idx[worst_idx]] == kMinScore) {
-      worst_idx -= 1;
-    }
-    const double wrost_score = score[sorted_idx[worst_idx]];
-    // start accmulate lambdas by pairs
+//
+//    // get the rank of the highest label (in this case there is only one relevant item)
+//    // this should simplify the calculation and maybe we can skip the inner loop
+    int r = 0;
     for (data_size_t i = 0; i < cnt; ++i) {
-      const data_size_t high = sorted_idx[i];
-      const int high_label = static_cast<int>(label[high]);
-      const double high_score = score[high];
-      if (high_score == kMinScore) { continue; }
-      const double high_label_gain = label_gain_[high_label];
-      const double high_discount = DCGCalculator::GetDiscount(i);
-      double high_sum_lambda = 0.0;
-      double high_sum_hessian = 0.0;
-      for (data_size_t j = 0; j < cnt; ++j) {
-        // skip same data
-        if (i == j) { continue; }
-
-        const data_size_t low = sorted_idx[j];
-        const int low_label = static_cast<int>(label[low]);
-        const double low_score = score[low];
-        // only consider pair with different label
-        if (high_label <= low_label || low_score == kMinScore) { continue; }
-
-        const double delta_score = high_score - low_score;
-
-        const double low_label_gain = label_gain_[low_label];
-        const double low_discount = DCGCalculator::GetDiscount(j);
-        // get dcg gap
-        const double dcg_gap = high_label_gain - low_label_gain;
-        // get discount of this pair
-        const double paired_discount = fabs(high_discount - low_discount);
-        // get delta NDCG
-        double delta_pair_NDCG = dcg_gap * paired_discount * inverse_max_dcg;
-        // regular the delta_pair_NDCG by score distance
-        if (high_label != low_label && best_score != wrost_score) {
-          delta_pair_NDCG /= (0.01f + fabs(delta_score));
+        if (label[sorted_idx[i]] > 0.1f) {
+            r = i;
+            break;
         }
-        // calculate lambda for this pair
-        double p_lambda = GetSigmoid(delta_score);
-        double p_hessian = p_lambda * (2.0f - p_lambda);
-        // update
-        p_lambda *= -delta_pair_NDCG;
-        p_hessian *= 2 * delta_pair_NDCG;
-        high_sum_lambda += p_lambda;
-        high_sum_hessian += p_hessian;
-        lambdas[low] -= static_cast<score_t>(p_lambda);
-        hessians[low] += static_cast<score_t>(p_hessian);
-      }
-      // update
-      lambdas[high] += static_cast<score_t>(high_sum_lambda);
-      hessians[high] += static_cast<score_t>(high_sum_hessian);
     }
+
+    // iterate only elements which are not relevant (all except r index element)
+    for (data_size_t j = 0; j < cnt; ++j) {
+      const data_size_t low = sorted_idx[j];
+      const data_size_t high = sorted_idx[r];
+      if (j == r || low > high) { continue; }
+      const double high_score = score[high];
+      const double low_score = score[low];
+      const double delta_score = high_score - low_score;
+      printf("delta_score=%.4f\n", delta_score);
+      const double delta_rr = 1.0f / (((static_cast<double>(low) + 1.0f)) - (1.0f / (static_cast<double>(high) + 1.0f)));
+      double p_lambda = 1.0f / (1.0f + std::exp(delta_score)); //GetSigmoid(delta_score);
+      double p_hessian = p_lambda * (2.0f - p_lambda);
+      // update
+      p_lambda *= delta_rr;
+      p_lambda = fabs(p_lambda);
+      p_hessian *= 2 * delta_rr;
+      lambdas[low] = static_cast<score_t>(p_lambda);
+      hessians[low] = static_cast<score_t>(p_hessian);
+    }
+
     // if need weights
     if (weights_ != nullptr) {
       for (data_size_t i = 0; i < cnt; ++i) {
         lambdas[i] = static_cast<score_t>(lambdas[i] * weights_[start + i]);
         hessians[i] = static_cast<score_t>(hessians[i] * weights_[start + i]);
       }
+    }
+
+    printf("------------\n");
+    for (data_size_t i = 0; i < cnt; ++i) {
+        printf("%d %.4f %.4f %.4f %.4f\n", i, label[sorted_idx[i]], score[sorted_idx[i]], lambdas[sorted_idx[i]], hessians[sorted_idx[i]]);
     }
   }
 
@@ -418,7 +468,7 @@ class LambdarankMRR: public ObjectiveFunction {
   }
 
   const char* GetName() const override {
-    return "lambdarank";
+    return "lambdarank_mrr";
   }
 
   std::string ToString() const override {
